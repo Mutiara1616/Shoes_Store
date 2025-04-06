9c09db0089a549279465ab20c0676458bad7ad74
fadlishidqi <fadlishidqifirdaus@gmail.com>
2025-04-06 22:42:56 +0700

    Fix Minor bugs

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -133,7 +133,7 @@
             </div>
             
             <div class="mt-8 flex justify-between">
-                <a href="{{ route('products.index') }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none">
+                <a href="{{ route('home') }}" class="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none">
                     <i class="fas fa-arrow-left mr-2"></i>
                     Continue Shopping
                 </a>
